4e50e16a68fa19541138221a4dd8893edd4d2d2b
anuradhavakil@gmail.com <anuradhavakil@yahoo.com>
2015-04-19 16:54:22 -0700

    Gumball game

---
+++ src/Display/score/me/Nohit.java
@@ -0,0 +1,18 @@
+
+package Display.score.me;
+
+
+public class Nohit extends Balls{
+    public Nohit(){
+        order = "Did not hit";
+    }
+    
+    public String getOrder()
+    {
+        return order;
+    }
+    
+    public int point(){
+        return 0;
+    }
+}
