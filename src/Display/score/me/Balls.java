9e6f5cd0086e16129473c388f4593c4be6a042fe
anuradhavakil@gmail.com <anuradhavakil@yahoo.com>
2015-04-19 17:07:57 -0700

    Revert "Revert "Gumball game""
    This reverts commit 9484b5fda3b0d8cf32e7fccfcd4991b629c160e7.

---
+++ src/Display/score/me/Balls.java
@@ -0,0 +1,15 @@
+
+package Display.score.me;
+
+public abstract class Balls {
+    String order = "Not Defined";
+    
+    public String getOrder(){
+        return order;
+    }
+    
+    public abstract int point();
+    
+    
+}
+
---
+++ src/Display/score/me/Color.java
@@ -0,0 +1,8 @@
+
+package Display.score.me;
+
+
+public abstract class Color extends Balls{
+    @Override
+    public abstract String getOrder();
+}
